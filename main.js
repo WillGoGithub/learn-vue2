4685f76b00133f44c223aafed97f2f7edc25d45e
+W <willohyayo@gmail.com>
2020-11-22 11:15:15 +0800

    inject/provide

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const app = Vue.createApp({
-
+    custom: 'hello',
+    inject: ['user'],
+    template: `
+        <div>
+            {{ user }}
+        </div>
+    `
 })
 
 app.config.errorHandler = (err, vm, info) => {
@@ -15,3 +21,33 @@ app.config.warnHandler = (msg, vm, trace) => {
 // Type: [key: string]: any
 // Default: undefined
 app.config.globalProperties.foo = 'bar'
+
+app.config.isCustomElement = tag => tag.startsWith('ico-')
+
+app.config.optionMergeStrategies.hello = (toVal, fromVal) => toVal || fromVal
+
+app.mixin({
+    custom: 'goodbye',
+    created() {
+        console.log(this.$option.custom)
+    },
+})
+
+// 開啟效能追蹤
+app.performance = true
+
+app.directive('focus', {
+    beforeMount() { },
+    mounted(el) {
+        el.focus()
+        // <input v-focus />
+    },
+    beforeUpdate() { },
+    updated() { },
+    beforeUnmount() { },
+    unmounted() { }
+})
+
+app.mount('#my-app')
+
+app.provide('user', 'admin')
